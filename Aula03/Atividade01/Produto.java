815c1798ba11dc8117d7b10b0edab7e506882256
Yuri Drescher <yuridrescheryd@gmail.com>
2026-08-17 21:31:09 -0300

    Create Produto class with name and price attributes

---
+++ Aula03/Atividade01/Produto.java
@@ -0,0 +1,11 @@
+package poo.semana03.atividade01;
+
+public class Produto {
+    String nome;
+    double preco;
+
+    public Produto(String nome, double preco) {
+        this.nome = nome;
+        this.preco = preco;
+    }
+}
