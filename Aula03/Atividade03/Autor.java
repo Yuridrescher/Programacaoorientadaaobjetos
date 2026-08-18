b51b1290fd94f714be789e43307aa0d624733e7d
Yuri Drescher <yuridrescheryd@gmail.com>
2026-08-17 21:38:13 -0300

    Add Autor class with name and nationality fields

---
+++ Aula03/Atividade03/Autor.java
@@ -0,0 +1,11 @@
+package poo.semana03.atividade03;
+
+public class Autor {
+    String nome;
+    String nacionalidade;
+
+    public Autor(String nome, String nacionalidade) {
+        this.nome = nome;
+        this.nacionalidade = nacionalidade;
+    }
+}
