caab66ee781d68c4e3a67f36855a0f11a1ce77d8
Yuri Drescher <yuridrescheryd@gmail.com>
2026-08-17 21:32:58 -0300

    Add Cliente class with name and email attributes

---
+++ Aula03/Atividade01/Cliente.java
@@ -0,0 +1,11 @@
+package poo.semana03.atividade01;
+
+public class Cliente {
+    String nome;
+    String email;
+
+    public Cliente(String nome, String email) {
+        this.nome = nome;
+        this.email = email;
+    }
+}
